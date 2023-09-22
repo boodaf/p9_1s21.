71dba495b2e350d99627307f3e4084fc37aed7cb
boodaf <144178847+boodaf@users.noreply.github.com>
2023-09-22 02:08:17 -0600

    Add files via upload

---
+++ p9_1s21.java
@@ -0,0 +1,23 @@
+import java.util.scanner;
+
+public class p9_1s21[
+    public class void main(string[] args) [
+    	  Scanner teclado = new Scanner(System.in);
+    	  String operacion;
+    	  System.out.println("Que tipo de operacion desea realizar");
+    	  System.out.println("escribala: Suma, Resta, Division, Multiplicacion");
+    	  operacion = teclado.nextLine();
+
+          if  (operacion.equals("Suma")) [
+          	       Sistem.out.println("Haremos una suma");
+          ] else if  (operacion.equals("Resta"))[
+          	       Sistem.out.println("Haremos una resta");
+          ] else if  (operacion.equals("Division"))[
+                   Sistem.out.equals("Haremos una division"));
+          ] else if  (operacion.equals("Multiplicacion"))[
+          	       Sistem.out.println("Haremos una multiplicacion");
+          ] else [
+                   Sistem.out.println("No exiete la operacion")       
+          ]	       
+    ]   
+]
